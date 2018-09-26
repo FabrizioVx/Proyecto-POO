5028e31c0ac2ad57b9b5e32438e2238810d9841d
Fabrizio <40775006+FabrizioVx@users.noreply.github.com>
2018-09-25 19:45:15 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -2,8 +2,35 @@
 // Created by Marko  Puchuri on 25/09/18.
 //
 
-#include <iostream>
-#include <fstream>
 #include <string>
 #include "funciones.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
+void QUIZ();
+
+int main()
+{
+    system("pause");
+    return 0;
+}
+
+void QUIZ(){
+    ofstream quiz;
+    
+    quiz.open("d://prueba/ejemplo.txt",ios::out);
+    
+    if(quiz.fail()){
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+    
+    quiz<<"Hola xd";
+    
+    quiz.close();
+    
+    
+    
+}
