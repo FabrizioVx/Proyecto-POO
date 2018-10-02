078f043dd0b79d0e2e054f4477776e63f1f6864c
markopuch <40008292+markopuch@users.noreply.github.com>
2018-10-02 13:31:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,26 +5,37 @@
 using namespace std;
 int main() {
     cout<<"SIMULADOR DE HORARIO\n";
+
     string nombre, apellido;
     cout<<"Proporcione su nombre: "; cin>>nombre;
     cout<<"Proporcione su apellido: "; cin>>apellido;
-    int puntos=test();//los puntos
-    cout<<"PUNTAJE TOTAL = "<<puntos<<endl;
+
+   // int puntos=test();//los puntos
+    //cout<<"PUNTAJE TOTAL = "<<puntos<<endl;
+
     int ciclo, horariopref;
+
     do{
-    cout<<"ingrese ciclo: ";cin>>ciclo;}
-    while(5<ciclo || ciclo<=0); //solo hasta quinto ciclo
-//imprimiendo horario
-    if (test()<30){//variado
-        burros(ciclo);//solo un horario
+        cout<<"//si incluye una letra lo tomaremos como ciclo 1\n";
+        cout<<"ingrese ciclo: ";
+        cin>>ciclo;
     }
+    while(5<ciclo || ciclo<0); //solo hasta quinto ciclo
+
+//imprimiendo horario
+    
+    if (test()<30){//variado
+        bajo(ciclo);//solo un horario
+    }
+
     if(test()>=30 && test()<70){
       cout<<"ingrese horario preferido : ";cin>>horariopref;//manana y tarde
-        normales(ciclo,horariopref);//
+        normal(ciclo,horariopref);//
     }
+
     if(test()>=70){
       cout<<"ingrese horario preferido: ";cin>>horariopref;//manana tarde y noche
-        cracks(ciclo,horariopref);
+        alto(ciclo,horariopref);
     }
     return 0;
 }
