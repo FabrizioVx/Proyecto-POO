ec632191d738f2bfc80f8424311598da7674351e
markopuch <40008292+markopuch@users.noreply.github.com>
2018-10-07 01:22:09 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,12 +1,17 @@
 //
 // Created by Marko  Puchuri on 25/09/18.
 //
-
+#include <iostream>
+#include <string>
+using namespace std;
 #ifndef PROYECTO_1_FUNCIONES_H
 #define PROYECTO_1_FUNCIONES_H
 int test();
+void eliminacion(char curso, string horario[8][5]);
 void bajo(int ciclo);
 void normal(int ciclo);
 auto verificacion_let(char letra);// verifica la letra del test debe ser a b c d si no volver a ingresar
 void alto(int ciclo);
+void imprimir(string horario[8][5]);
+
 #endif //PROYECTO_1_FUNCIONES_H
