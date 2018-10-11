e47fb42498f26e875b44ca26b4980af90631c780
markopuch <40008292+markopuch@users.noreply.github.com>
2018-10-11 01:07:37 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -13,6 +13,7 @@ void normal(int ciclo);
 auto verificacion_let(char letra);// verifica la letra del test debe ser a b c d si no volver a ingresar
 void alto(int ciclo);
 void imprimir(string horario[8][5]);
+int inden();
 
 
 #endif //PROYECTO_1_FUNCIONES_H
