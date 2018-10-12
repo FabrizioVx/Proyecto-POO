ebf6a9bc9ed6a10259322dba188ec777e373c15e
markopuch <40008292+markopuch@users.noreply.github.com>
2018-10-12 00:13:42 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -7,14 +7,14 @@ using namespace std;
 #ifndef PROYECTO_1_FUNCIONES_H
 #define PROYECTO_1_FUNCIONES_H
 int test();
-void eliminacion(char curso, string horario[8][5]);//eliminacion de los cursos
+void eliminacion(string curso, string matriz[8][5]);//eliminacion de los cursos
 void bajo(int ciclo);
 void normal(int ciclo);
 void leerarch(string a[8][5]);//guardando archivo
 void alto(int ciclo);
 void imprimir(string horario[8][5]);
 int inden();//Verificador de los numeros
-string horario(char curso, int ciclo);
+string horarios( int ciclo,char curso);
 
 
 #endif //PROYECTO_1_FUNCIONES_H
