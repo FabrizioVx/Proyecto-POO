610c2e37ad7db32c3f5e15b13239d103f02eb85c
Fabrizio <40775006+FabrizioVx@users.noreply.github.com>
2018-10-12 19:12:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main() {
     cout<<"HORARIO\n";
 
     string nombre, apellido;
-    cout<<"Proporcione su nombre: "; cin>>nombre;
-    cout<<"Proporcione su apellido: "; cin>>apellido;
+    cout<<"Proporcione su nombre: "; getline(cin,nombre);
+    cout<<"Proporcione su apellido: "; getline(cin,apellido);
 
     int puntos=test();//los puntos
     cout<<"PUNTAJE TOTAL = "<<puntos<<endl;
@@ -32,11 +32,11 @@ int main() {
     }
 
     if(puntos>=30 && puntos<70){
-        normal(ciclo);
+        normal(ciclo);//entr a la funcion normal
     }
 
     if(puntos>=70){
-        alto(ciclo);
+        alto(ciclo);//entra a la funcion alto
     }
     return 0;
 }
